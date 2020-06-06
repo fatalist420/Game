487ae4aeb274251976039fb353e7631958d2af2b
Fatalist420 <fatalist420@gmail.com>
2020-06-06 19:21:19 +0300

    j

--- app/src/main/java/ru/dp/game/MainActivity.kt
+++ app/src/main/java/ru/dp/game/MainActivity.kt
@@ -1,63 +1,41 @@
 package ru.dp.game
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.android.synthetic.main.activity_login.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val userName:String = "KINTOHO"
-    val userPassword:String = "A123"
-    var userNameBoolean : Boolean = false
-    var userPasswordBoolean :Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val watcherUser = object : TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-                val inputUserName = s.toString().toUpperCase(Locale.ROOT)
-                //Toast.makeText(this@MainActivity, "after $inputUser", Toast.LENGTH_SHORT).show()
-                userNameBoolean = if (inputUserName == userName) {
-                    println("User name is VALID")
-                    true
-                } else false
-                if (userNameBoolean&&userPasswordBoolean) println("Start New ACTIVITY")
-            }
+        val LoginTerminal = Intent(this@MainActivity, LoginTerminalActivity::class.java)
+        startActivityForResult(LoginTerminal, 1)
+    }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                //Toast.makeText(this@MainActivity, "befor Chenged $s", Toast.LENGTH_SHORT).show()
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                //Toast.makeText(this@MainActivity, "on Chenged $s", Toast.LENGTH_SHORT).show()
-            }
+    // Обработка двойного нажатия кнопки "BACK" для выхода из приложения
+    private var back_press: Long = 0
+
+    override fun onBackPressed() {
+        if (back_press + 1000 > System.currentTimeMillis()) {
+            super.onBackPressed()
+        } else {
+            back_press = System.currentTimeMillis()
+            Toast.makeText(
+                baseContext,
+                " Для выхода нажмите кнопку Назад еще раз",
+                Toast.LENGTH_SHORT
+            ).show()
         }
-
-        val watcherPass = object : TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-                val inputUserPassword = s.toString().toUpperCase(Locale.ROOT)
-                userPasswordBoolean = if (inputUserPassword == userPassword) {
-                    println("User pass is VALID")
-                    true
-                } else false
-                if (userNameBoolean&&userPasswordBoolean) println("Start New ACTIVITY")
-                //Toast.makeText(this@MainActivity, "after Chenged $s", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                //Toast.makeText(this@MainActivity, "befor Chenged $s", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                //Toast.makeText(this@MainActivity, "on Chenged $s", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        iTUserName.addTextChangedListener(watcherUser)
-        iTUserPass.addTextChangedListener(watcherPass)
     }
 }
+
---
+++ app/src/main/java/ru/dp/game/TerminalActivity.kt
@@ -0,0 +1,13 @@
+package ru.dp.game
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class TerminalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terminal)
+    }
+}
